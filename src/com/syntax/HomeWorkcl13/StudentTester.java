f88651c1b268548cc11a1f3db445ee84e11c8bb6
annewynter <kushnareva86@gmail.com>
2021-08-18 13:54:00 -0400

    second attempt

--- src/com/syntax/HomeWorkcl13/StudentTester.java
+++ src/com/syntax/HomeWorkcl13/StudentTester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StudentTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 
 
 		Student input=new Student();
 		Scanner scan;
